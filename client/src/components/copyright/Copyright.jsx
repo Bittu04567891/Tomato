97405563473687766c7bfc5c5f54e44eeb5a2abc
Bittu <kumarbittu775@gmail.com>
2023-12-18 16:21:08 +0530

    footer

--- client/src/components/copyright/Copyright.jsx
+++ client/src/components/copyright/Copyright.jsx
@@ -6,7 +6,8 @@ const Copyright = () => {
       <hr />
       By continuing past this page, you agree to our Terms of Service, Cookie
       Policy, Privacy Policy and Content Policies. All trademarks are properties
-      of their respective owners. 2008-2023 © Zomato™ Ltd. All rights reserved.
+      of their respective owners. 2008-2023 © TomatoEats™ Ltd. All rights
+      reserved.
     </div>
   );
 };
